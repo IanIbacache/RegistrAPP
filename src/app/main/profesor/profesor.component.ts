984bf9ba8e2eba96e0dc37ba6e0c175e473ac379
BrianMartelA <brianmartelabdala@gmail.com>
2024-09-24 19:59:05 -0300

    Modificacion Detalles

--- src/app/main/profesor/profesor.component.ts
+++ src/app/main/profesor/profesor.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profesor',
   templateUrl: './profesor.component.html',
   styleUrls: ['./profesor.component.scss'],
 })
-export class ProfesorComponent  implements OnInit {
+export class ProfesorComponent  implements OnInit, OnDestroy {
+
+  username: string;
+  private authService = inject(AuthService);
+
+  subscriptionAuthService: Subscription;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscriptionAuthService = this.authService.username$.subscribe(username => {
+      this.username = username
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscriptionAuthService?.unsubscribe();
+  }
 
 }
